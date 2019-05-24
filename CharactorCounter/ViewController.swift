7d2ac147f1a64595be72cd1140bfbcb68d021f59
jun.yamada <jun.yamada@rakuten.com>
2019-05-24 17:08:42 +0900

    Initial Commit

---
+++ CharactorCounter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CharactorCounter
+//
+//  Created by Yamada, Jun | Junkey | OSPD on 2019/05/24.
+//  Copyright © 2019 jun yamada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
